dcca502c4dd755ccea4e2d52297406eaac756bda
Dev Govindji <friendsofdev94@gmail.com>
2020-09-30 13:05:52 -0500

    destructures tresponses to objects

--- src/app/fetch-trefle.service.ts
+++ src/app/fetch-trefle.service.ts
@@ -2,28 +2,14 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { pluck, tap, concatMap, mergeMap, map, filter, first, elementAt } from 'rxjs/operators'
-import { of } from 'rxjs'
-
-interface GrowthData {
-  id: number
-  common_name: string
-  scientific_name: string
-  growth: {
-    minimum_precipitation: {
-      mm: number
-    }
-    maximum_precipitation: {
-      mm: number
-    }
-  }
-}
-
 
 // create new data type to store returns from both requests
 // create button that when clicked makes request for growth info, sending back id -- COMPLETE
 // pass id from plantInfo to plantGrowth -- COMPLETE
 // turn response from array to objects
 // show multiple getPlantImageIdName responses
+
+// only shows last element in PlantInfo object - check why that it is
 
 
 @Injectable({
@@ -33,7 +19,6 @@ export class FetchTrefleService {
 
   constructor(private http: HttpClient) { }
 
-  plantId: number
 
   url: any = 'https://trefle.io/api/v1/plants?token=GTF4gOKNDJTmYmR2ut6r6y1fyD3pN1GrGSEoST_s0mA'
 
@@ -41,21 +26,19 @@ export class FetchTrefleService {
 
   page: any = '&page=1'
 
-  id
-  common_name
-  scientific_name
-  maximum_precipitation
-  minimum_precipitation
-  image_url
 
-  growth: {
-    minimum_precipitation: {
-      mm
-    }
-    maximum_precipitation: {
-      mm
-    }
+  plantInfo = {
+    plantId: 0,
+    common_name: '',
+    image_url: '',
+    id: 0,
+    scientific_name: '',
+    maximum_precipitation: '',
+    minimum_precipitation: '',
   }
+
+
+
 
 
   // fetchAllPlantData(): Observable<any> {
@@ -85,18 +68,23 @@ export class FetchTrefleService {
         pluck("data"),
         map((item: any) => {
           // item = item.splice(0, 10)
-          return [this.plantId, this.common_name, this.image_url] = [item[0].id, item[0].common_name, item[0].image_url]
+          // return (item[0].id, item[0].common_name, item[0].image_url = { ...this.plantInfo })
+          return (this.plantInfo.plantId = item[0].id, this.plantInfo.common_name = item[0].common_name, this.plantInfo.image_url = item[0].image_url)
+          // return [this.plantId, this.common_name, this.image_url] = [item[0].id, item[0].common_name, item[0].image_url]
         })
       )
   }
 
   getPlantGrowth(): Observable<any> {
-    return this.http.get(this.proxyurl + 'https://trefle.io/api/v1/plants/' + this.plantId + '?token=GTF4gOKNDJTmYmR2ut6r6y1fyD3pN1GrGSEoST_s0mA' + this.page)
+    return this.http.get(this.proxyurl + 'https://trefle.io/api/v1/plants/' + this.plantInfo.plantId + '?token=GTF4gOKNDJTmYmR2ut6r6y1fyD3pN1GrGSEoST_s0mA' + this.page)
       .pipe(
         pluck("data"),
         pluck("main_species"),
         map((item: any) => {
-          return [this.id, this.common_name, this.scientific_name, this.maximum_precipitation, this.minimum_precipitation] = [item.id, item.common_name, item.scientific_name, item.growth.maximum_precipitation.mm, item.growth.minimum_precipitation.mm]
+          return (item.id, item.common_name, item.scientific_name, item.growth.maximum_precipitation.mm, item.growth.minimum_precipitation.mm = { ...this.plantInfo })
+
+          // return (this.plantInfo.id = item.id, this.plantInfo.common_name = item.common_name, this.plantInfo.scientific_name = item.scientific_name, this.plantInfo.maximum_precipitation = item.growth.maximum_precipitation.mm, this.plantInfo.minimum_precipitation = item.growth.minimum_precipitation.mm)
+          // return [this.id, this.common_name, this.scientific_name, this.maximum_precipitation, this.minimum_precipitation] = [item.id, item.common_name, item.scientific_name, item.growth.maximum_precipitation.mm, item.growth.minimum_precipitation.mm]
         })
       )
   }
