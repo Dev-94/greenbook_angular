de74b563164bbb5c491b5ee927f437bbc5c4d98e
Dev Govindji <friendsofdev94@gmail.com>
2020-10-12 10:30:48 -0500

    removes dead code, stups page change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,9 @@ import { FetchTrefleService } from './fetch-trefle.service'
 // Create search functionality --- COMPLETED
 // Create clear field for search --- COMPLETED
 // Search API for search --- COMPLETED
+// create page button
 // get growth info for queryPlants
 // get growth info for all plants
-// create page button
 // how to pass id to Growth()
 
 @Component({
@@ -18,23 +18,6 @@ import { FetchTrefleService } from './fetch-trefle.service'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  // state data, splice to get through number indices, use item to filter out all except name, id, image, return objects in an array
-  /*
-  data:
-   0:
-
-    common_name: "Red fescue"
-    id: 137025
-    image_url: "https://bs.floristic.org/image/o/d073c0ecfeb2f69248e9102eb6ec10f8ccc628cb"
-
-   1:
-
-    common_name: "Creeping buttercup"
-    id: 173910
-    image_url: "https://bs.floristic.org/image/o/c6d9a5222b6ef0e3a7bdef3350278718d3097bce"
-
-  */
 
 
   objIdNameImage: any
@@ -51,50 +34,32 @@ export class AppComponent {
 
   queryPlants: any
 
+  page = 1
+
+
 
   constructor(private fetchTrefleService: FetchTrefleService) { }
 
 
   handleQuery(input) {
     this.query = input
-    console.log('handleQuery()', this.query)
-    // send this.query as argument to service function
-    // service function is the url with the search term, presumably
-    // depends on docs
-    // look at docs on how to search
-    // http://{defaultHost}/api/v1/plants/search + this.q + this.token
-    // this.q is the query being passed down to the service
-    // may have issues with bubbling data back up to ui (data binding)
     this.fetchTrefleService.getPlantsForQuery(this.query).subscribe(res => console.log(this.queryPlants = res))
   }
 
+  nextPage() {
 
-  // I need an array of objects
-  // obj should have keys of id, name, image
-  // the array should then be listed in template
+  }
+
+
   fetchPlantInfo(): void {
-    this.fetchTrefleService.getPlantImageIdName().subscribe(res => {
-      console.log('getPlantImageIdName()', res)
-      // var results = res.data.splice(0, 5)
-      // this.objIdNameImage = results
-      // console.log('results', results)
-      // id is not an array, it inside an object that is within an array
-      // results.map(item => {
-      //   this.id.push(item.id)
-      //   this.name.push(item.common_name)
-      //   this.image.push(item.image_url)
-
-      // })
-      // console.log('id, name, image', [this.id, this.name, this.image])
+    this.fetchTrefleService.getPlantImageIdName(this.page).subscribe(res => {
       this.plantInfo = res
 
     })
   }
 
   fetchPlantGrowth(): void {
-    this.fetchTrefleService.getPlantGrowth().subscribe(res => {
-      console.log('getPlantGrowth()', res)
-      // this.plantGrowth = res
+    this.fetchTrefleService.getPlantGrowth(this.page).subscribe(res => {
       this.scientific_name = res.data.main_species.scientific_name,
         this.maximum_precipitation = res.data.main_species.growth.maximum_precipitation.mm,
         this.minimum_precipitation = res.data.main_species.growth.minimum_precipitation.mm,
--- src/app/fetch-trefle.service.ts
+++ src/app/fetch-trefle.service.ts
@@ -1,21 +1,6 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
-import { pluck, tap, concatMap, mergeMap, map, filter, first, elementAt } from 'rxjs/operators'
-
-// create new data type to store returns from both requests
-// create button that when clicked makes request for growth info, sending back id -- COMPLETE
-// pass id from plantInfo to plantGrowth -- COMPLETE
-// turn response from array to objects
-// show multiple getPlantImageIdName responses
-// create button to switch to next plant --- COMPLETE
-
-// only shows last element in PlantInfo object - check why that is
-// shows as string, not object
-
-// Get rid of logic from first call
-// get values from comoponet.ts
-// send back id when call is made from template
 
 interface PlantInfo {
   plantId: number
@@ -40,7 +25,7 @@ export class FetchTrefleService {
 
   proxyurl: any = 'https://cors-anywhere.herokuapp.com/'
 
-  page: any = '&page=1'
+  // page: any = '&page=1'
 
   plantInfo = {
     plantId: 1164724,
@@ -61,56 +46,15 @@ export class FetchTrefleService {
   }
 
 
-  getPlantImageIdName(): Observable<any> {
-    // data.common_name, data.image_url, data.main_species_id
-    return this.http.get(this.proxyurl + this.url + this.page)
-    // .pipe(
-    //   pluck('data'),
-    //   map((item: any) => {
 
-    // REDO AND RETURN OBJECT WITH UPDATED VALUES FOR EACH KEY
 
-    // return (
-    //   this.plantInfo.plantId = item[this.i].id,
-    //   this.plantInfo.common_name = item[this.i].common_name,
-    //   this.plantInfo.image_url = item[this.i].image_url
-    // )
-
-    // return (
-    //   item[this.i].id,
-    //   item[this.i].common_name,
-    //   item[this.i].image
-    //   = this.plantInfo
-    // )
-
-    // return (item[this.i].plantId, item[this.i].common_name, item[this.i].image_url = { this.plantInfo.plantId, this.plantInfo.plantId, this.plantInfo.plantId })
-
-    // return ([this.plantInfo.plantId, this.plantInfo.common_name, this.plantInfo.image] = [item[this.i].id, item[this.i].common_name, item[this.i].image_url])
-    // return ([this.plantInfo.plantId, this.plantInfo.common_name, this.plantInfo.image] = [item[this.i].id, item[this.i].common_name, item[this.i].image_url])
-
-    // })
-    // )
+  getPlantImageIdName(page): Observable<any> {
+    return this.http.get(this.proxyurl + this.url + '&page=' + page)
 
   }
 
-  getPlantGrowth(): Observable<any> {
-    return this.http.get(this.proxyurl + 'https://trefle.io/api/v1/plants/' + 137025 + '?token=GTF4gOKNDJTmYmR2ut6r6y1fyD3pN1GrGSEoST_s0mA' + this.page)
-    // .pipe(
-    //   pluck('data'),
-    //   pluck('main_species'),
-    //   map((item: any) => {
-    //     return (
-    //       item.scientific_name,
-    //       item.growth.maximum_precipitation.mm,
-    //       item.growth.minimum_precipitation.mm
-    //       = { ...this.plantGrowth }
-    //     )
-
-    // return (this.plantInfo.id = item.id, this.plantInfo.common_name = item.common_name, this.plantInfo.scientific_name = item.scientific_name, this.plantInfo.maximum_precipitation = item.growth.maximum_precipitation.mm, this.plantInfo.minimum_precipitation = item.growth.minimum_precipitation.mm)
-
-    // return [this.id, this.common_name, this.scientific_name, this.maximum_precipitation, this.minimum_precipitation] = [item.id, item.common_name, item.scientific_name, item.growth.maximum_precipitation.mm, item.growth.minimum_precipitation.mm]
-    // })
-    // )
+  getPlantGrowth(page): Observable<any> {
+    return this.http.get(this.proxyurl + 'https://trefle.io/api/v1/plants/' + 137025 + '?token=GTF4gOKNDJTmYmR2ut6r6y1fyD3pN1GrGSEoST_s0mA' + page)
   }
 
   getNextPlant(): void {
